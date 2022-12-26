f42483c36d4392340bf481eccc7c28bdf1f118ce
henriquestiagoo <henriquestiago@ua.pt>
2022-12-26 21:39:26 +0000

    Changed dependency version target

--- ReadMe/Package.swift
+++ ReadMe/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.4.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
