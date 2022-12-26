e243d97314cfb7ac771ca52e26b36e3e92ec6331
henriquestiagoo <henriquestiago@ua.pt>
2022-12-26 21:43:17 +0000

    Reverted changes

--- ReadMe/Package.swift
+++ ReadMe/Package.swift
@@ -9,25 +9,20 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.4.0")),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .executableTarget(
             name: "ReadMe",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]
         ),
-//        .executableTarget(
-//            name: "ReadMe",
-//            dependencies: [
-//                .product(name: "ArgumentParser", package: "swift-argument-parser")
-//            ]
-//        ),
-//        .testTarget(
-//            name: "ReadMeTests",
-//            dependencies: ["ReadMe"]),
+        .testTarget(
+            name: "ReadMeTests",
+            dependencies: ["ReadMe"]
+        ),
     ]
 )
