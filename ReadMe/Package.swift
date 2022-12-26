e05b5b59638c7c9c1d749732774d0a5dfdbd200b
henriquestiagoo <henriquestiago@ua.pt>
2022-12-26 21:34:42 +0000

    Changed executableTarget to target

--- ReadMe/Package.swift
+++ ReadMe/Package.swift
@@ -14,14 +14,20 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
+        .target(
             name: "ReadMe",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]
         ),
-        .testTarget(
-            name: "ReadMeTests",
-            dependencies: ["ReadMe"]),
+//        .executableTarget(
+//            name: "ReadMe",
+//            dependencies: [
+//                .product(name: "ArgumentParser", package: "swift-argument-parser")
+//            ]
+//        ),
+//        .testTarget(
+//            name: "ReadMeTests",
+//            dependencies: ["ReadMe"]),
     ]
 )
