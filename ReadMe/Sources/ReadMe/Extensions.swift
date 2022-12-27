bcbe42a4e79d43698c069b3ae7560e27a2bd1f1e
henriquestiagoo <henriquestiago@ua.pt>
2022-12-27 18:33:20 +0000

    Github actions playground v1

--- ReadMe/Sources/ReadMe/Extensions.swift
+++ ReadMe/Sources/ReadMe/Extensions.swift
@@ -15,3 +15,20 @@ public extension JSONDecoder {
         return decoder
     }()
 }
+
+@available(macOS, deprecated: 12.0, message: "Use the built-in API instead")
+extension URLSession {
+    func data(from urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: urlRequest) { data, response, error in
+                guard let data = data, let response = response else {
+                    let error = error ?? URLError(.badServerResponse)
+                    return continuation.resume(throwing: error)
+                }
+
+                continuation.resume(returning: (data, response))
+            }
+            task.resume()
+        }
+    }
+}
